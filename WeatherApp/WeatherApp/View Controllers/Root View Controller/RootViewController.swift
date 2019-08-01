3ddea2b999315a6c40bbbdbc31e5e1efc9cd172f
Ramon Geronimo <rgero215@gmail.com>
2019-08-01 10:43:04 -0700

    project setup

--- WeatherApp/WeatherApp/View Controllers/Root View Controller/RootViewController.swift
+++ WeatherApp/WeatherApp/View Controllers/Root View Controller/RootViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class RootViewController: UIViewController {
+    
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
