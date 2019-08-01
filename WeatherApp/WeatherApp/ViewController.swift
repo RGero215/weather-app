374683c558156fe026a085a0020feb556545fdcc
Ramon Geronimo <rgero215@gmail.com>
2019-08-01 10:27:46 -0700

    started take home project

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Ramon Geronimo on 8/1/19.
+//  Copyright © 2019 Ramon Geronimo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
